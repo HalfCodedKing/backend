b7548063cafb00e1c9e16127d22dbcedf60ba6b1
Hardik Kakkar <hardikkakkar25@gmail.com>
2023-12-25 22:46:50 +0530

    init

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(cors({
 }));
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
+app.options('*', cors());
 app.use('/users/', usersRoutes);
 app.use('/admin/', adminRoutes);
 app.use('/franchise/', franchiseRoutes);
