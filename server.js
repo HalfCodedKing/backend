d049de2d37f7442da08888081e5ef82c83e7d993
Hardik Kakkar <hardikkakkar25@gmail.com>
2023-12-25 22:32:13 +0530

    cors fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const jwtSecret = 'lecturevecture';
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["*"],
   methods: ['GET', 'POST','PUT','DELETE'],
   credentials: true
 }));
